766e3e457adb365bf5d87a0369200643d7c4fb0c
joseluiscaballerov <joseluiscaballerow@gmail.com>
2023-08-06 14:20:26 +0200

    Despliegue inicial en Heroku

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
 const { Client, Intents, MessageAttachment } = require('discord.js');
 const { createCanvas, loadImage } = require('canvas');
 const config = require('./config.js');
+const express = require('express');
+
 const TOKEN = config.token;
+const PORT = process.env.PORT || 3000; // Usar el puerto proporcionado por Heroku o el puerto 3000 si se ejecuta localmente
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
+const app = express();
 
 // Mensaje de bienvenida que se enviará a los nuevos usuarios
 const MENSAJE_BIENVENIDA = `
@@ -19,41 +23,41 @@ X ▶ https://www.x.com/josecaballerow
 
 client.once('ready', () => {
   console.log(`Bot conectado como ${client.user.tag}`);
+  // El bot está listo, así que ahora escuchemos en el puerto proporcionado por Heroku
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  });
 });
 
-// Usa "once" en lugar de "on" para el evento "guildMemberAdd"
-client.once('guildMemberAdd', async (member) => {
-  // Comprueba si el usuario es un bot, si es así, no enviar el mensaje de bienvenida
-  if (member.user.bot) return;
+client.on('guildMemberAdd', async (member) => {
+    // Crear una imagen personalizada con el nombre y el avatar del usuario
+    const canvas = createCanvas(800, 200);
+    const ctx = canvas.getContext('2d');
 
-  // Crear una imagen personalizada con el nombre y el avatar del usuario
-  const canvas = createCanvas(800, 200);
-  const ctx = canvas.getContext('2d');
+    // Fondo
+    ctx.fillStyle = '#f1c40f'; // Color amarillo suave
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Fondo
-  ctx.fillStyle = '#f1c40f'; // Color amarillo suave
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Avatar del usuario
+    const avatarSize = 150;
+    const avatarX = 20;
+    const avatarY = canvas.height / 2 - avatarSize / 2;
+    const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png' }));
+    ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
 
-  // Avatar del usuario
-  const avatarSize = 150;
-  const avatarX = 20;
-  const avatarY = canvas.height / 2 - avatarSize / 2;
-  const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png' }));
-  ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+    // Nombre del usuario
+    ctx.fillStyle = '#000000'; // Color negro
+    ctx.font = '40px "Segoe UI", sans-serif'; // Fuente Segoe UI (o elige una fuente bonita)
+    ctx.textAlign = 'left';
+    ctx.fillText(member.user.username, avatarX + avatarSize + 20, canvas.height / 2 + 15);
 
-  // Nombre del usuario
-  ctx.fillStyle = '#000000'; // Color negro
-  ctx.font = '40px "Segoe UI", sans-serif'; // Fuente Segoe UI (o elige una fuente bonita)
-  ctx.textAlign = 'left';
-  ctx.fillText(member.user.username, avatarX + avatarSize + 20, canvas.height / 2 + 15);
+    // Crear una imagen adjunta con la imagen personalizada
+    const attachment = new MessageAttachment(canvas.toBuffer(), 'bienvenida.png');
 
-  // Crear una imagen adjunta con la imagen personalizada
-  const attachment = new MessageAttachment(canvas.toBuffer(), 'bienvenida.png');
-
-  // Enviar mensaje de bienvenida personalizado al usuario con la imagen adjunta
-  member.send({ content: MENSAJE_BIENVENIDA, files: [attachment] })
-    .then(() => console.log(`Mensaje de bienvenida enviado a ${member.user.tag}`))
-    .catch((error) => console.error(`No se pudo enviar el mensaje directo a ${member.user.tag}. Error: ${error}`));
+    // Enviar mensaje de bienvenida personalizado al usuario con la imagen adjunta
+    member.send({ content: MENSAJE_BIENVENIDA, files: [attachment] })
+        .then(() => console.log(`Mensaje de bienvenida enviado a ${member.user.tag}`))
+        .catch((error) => console.error(`No se pudo enviar el mensaje directo a ${member.user.tag}. Error: ${error}`));
 });
 
 // Iniciar sesión en Discord con el token del bot
